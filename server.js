237edf61ce41ee859d933776de51f5798ecdbedf
victoria-cameron <victoria.cameron2@marist.edu>
2019-11-21 19:29:42 -0500

    initial push
    Site base files- first push

---
+++ server.js
@@ -0,0 +1,22 @@
+﻿const express = require('express');
+const app = express();
+var cons = require('consolidate');
+var path = require('path');
+
+// view engine setup
+app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Homepage',
+    });
+});
+
+
+const server = app.listen(7000, () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
